a63242b9a66ac0015b1d81c2d8e314b9c29f7213
SayakGithub01 <sayak.majumdermca@gmail.com>
2021-04-11 20:49:52 +0530

    added */

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello
 {
     public static void main(String[] args)
     {
-        System.out.println("+-");
+        System.out.println("+-*/");
         System.out.println("https://github.com/SayakGithub01/MavenBuild.git");
     }
 }
